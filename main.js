dc8b5a74d515a0da9b7f151392d9afbcbeb00941
maza62RUS <mazay62rus@gmail.com>
2017-03-13 22:14:04 +0300

    anal

---
+++ main.js
@@ -0,0 +1,40 @@
+var s = Snap('#svg');
+var svg = document.getElementById('svg');
+var style = {
+	fill: randFill(1,6),
+	stroke: '#eee',
+	strokeWidth: 5	
+};
+
+s.click(function(e){
+	createCircle(e);
+});
+
+function createCircle(e) {
+	if(e.target.tagName == 'svg') {
+		var circle = s.circle(e.offsetX,e.offsetY,35);
+		circle.attr(style);
+	} else {
+		console.log('eee blet');
+	}
+	circle.hover(function(){
+		circle.animate(circle.attr({stroke:'silver'}), 500)
+		console.log('hover');
+	});
+	circle.unhover(function(){
+		circle.animate(circle.attr({stroke:'#eee'}), 500);
+		console.log('unhover');
+	});
+
+};
+
+function randFill(min,max) {
+	var num = Math.floor(Math.random() * (max - min)) + min;
+	var color;
+	if (num == 1) color = 'tomato';
+	if (num == 2) color = 'lightblue';
+	if (num == 3) color = 'lightgreen';
+	if (num == 4) color = 'coral';
+	if (num == 5) color = 'white';
+	return color;
+};
